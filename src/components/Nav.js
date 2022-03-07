5cb7d452ed0f79bf6e3bab088c8f1d741c98d914
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-03-07 13:01:15 +0100

    add flags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import hunFlag from "../001-hungary.png";
+import enFlag from "../001-united-kingdom.png";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [language, setLanguage] = useState("hu");
+
+  const handleOnclick = (e) => {
+    e.preventDefault();
+    setLanguage(e.target.value);
+  };
   return (
     <nav className="navbar">
       <div className="container">
@@ -18,6 +26,16 @@ const Nav = () => {
           </li>
           <li>
             <p>Kapcsolat</p>
+          </li>
+          <li>
+            <button
+              value={language === "en" ? "hu" : "en"}
+              onClick={() => {
+                handleOnclick();
+              }}
+            >
+              <img src={enFlag} alt="language"/>
+            </button>
           </li>
         </ul>
       </div>

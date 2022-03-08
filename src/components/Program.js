dd879c19af0cc5fc59b57431ee2e2ed63f737970
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-03-08 22:20:41 +0100

    refactor en program to table

--- src/components/Program.js
+++ src/components/Program.js
@@ -7,7 +7,6 @@ const Program = ({ currentLanguage }) => {
         <div className="container">
           <div className="program">
             <h1>PROGRAMOK</h1>
-
             <table>
               <tbody>
                 <tr>
@@ -89,38 +88,82 @@ const Program = ({ currentLanguage }) => {
         <div className="container">
           <div className="program">
             <h1>PROGRAMMES</h1>
-            <h4>6:30-8:30</h4>
-            <p>Arrival and parking of buses</p>
-            <h4>9:00</h4>
-            <p>Opening ceremony</p>
-            <h4>9:30-14:00</h4>
-            <p>
-              Nostalgic city-bus services operate in town. This is a non
-              stopping service so boarding and disembarking is only allowed at
-              the Köztársaság square bus stop (event site) with a redeemed
-              ticket. Keep in mind that the capacity of the old timer vehicles
-              is limited so to avoid any incidents please cooperate with the
-              ticket controllers when boarding a bus! The tickets are valid for
-              one round only!
-            </p>
-
-            <p>Route of the nostalgic city-bus services:</p>
-            <p>On the map:</p>
-            <p>Route of the nostalgic city-bus services</p>
-            <h4>14:30</h4>
-            <p>Old timer bus parade</p>
-            <p>
-              Old timer bus parade featuring all attending vehicles in the
-              picturesque Káli-basin on the
-              Tapolca-Gyulakeszi-Kékkút-Kővágóörs-Köveskál-Gyulakeszi-Tapolca
-              route:
-            </p>
-            <p>On the map:</p>
-            <p>Route of the parade</p>
-            <h4>16:20</h4>
-            <p>Tombola draw, musical evening</p>
-            <h4>23:00</h4>
-            <p>Event closing, vehicles leaving</p>
+            <table>
+              <tbody>
+                <tr>
+                  <th className="tableWidth"></th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>6:30-8:30</h4>
+                  </td>
+                  <td>
+                    <strong>Arrival and parking of buses</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>9:00</h4>
+                  </td>
+                  <td>
+                    <strong>Opening ceremony</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>9:30-14:00</h4>
+                  </td>
+                  <td>
+                    <p>
+                      Nostalgic city-bus services operate in town. This is a non
+                      stopping service so boarding and disembarking is only
+                      allowed at the Köztársaság square bus stop (event site)
+                      with a redeemed ticket. Keep in mind that the capacity of
+                      the old timer vehicles is limited so to avoid any
+                      incidents please cooperate with the ticket controllers
+                      when boarding a bus! The tickets are valid for one round
+                      only!
+                    </p>
+                    <p>Route of the nostalgic city-bus services:</p>
+                    <p>On the map:</p>
+                    <p>Route of the nostalgic city-bus services</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>14:30</h4>
+                  </td>
+                  <td>
+                    <strong>Old timer bus parade</strong>
+                    <p>
+                      Old timer bus parade featuring all attending vehicles in
+                      the picturesque Káli-basin on the
+                      Tapolca-Gyulakeszi-Kékkút-Kővágóörs-Köveskál-Gyulakeszi-Tapolca
+                      route:
+                    </p>
+                    <p>On the map:</p>
+                    <p>Route of the parade</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>16:20</h4>
+                  </td>
+                  <td>
+                    <strong>Tombola draw, musical evening</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>23:00</h4>
+                  </td>
+                  <td>
+                    <strong>Event closing, vehicles leaving</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       )}

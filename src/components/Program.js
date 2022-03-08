34eea31cb717cc4a0f2a2d31adf417b2959142e3
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-03-08 21:40:11 +0100

    refactor hu program to table

--- src/components/Program.js
+++ src/components/Program.js
@@ -7,36 +7,82 @@ const Program = ({ currentLanguage }) => {
         <div className="container">
           <div className="program">
             <h1>PROGRAMOK</h1>
-            <h4>6:30-8:30</h4>
-            <p>Autóbuszok érkezése, beállítása</p>
-            <h4>9:00</h4>
-            <p>Megnyitó</p>
-            <h4>9:30-14:00</h4>
-            <p>
-              Városi nosztalgia járatok közlekednek, melyekre a Köztársaság téri
-              kijelölt autóbusz megállóhelyen lehet felszállni előre megváltott
-              menetjeggyel. A járatoknak útközben nincs megállóhelye, le-,
-              illetve felszállás csak a végállomáson lehetséges. Az autóbuszok
-              férőhelye korlátozott, kérjük ezt vegyék figyelembe felszálláskor
-              és a problémamentes utazásért a kalauzok kéréseit tartsák be.
-            </p>
-            <p>A járatok az alábbi útvonalon közlekednek:</p>
-            <p>Térképen:</p>
-            <p>Városi nosztalgia autóbusz körjáratok útvonala</p>
-            <h4>14:30</h4>
-            <p>Felvonulás</p>
-            <p>
-              Felvonulás az összes jármű részvételével a a festői szépségű
-              Káli-Medencében
-              Tapolca-Gyulakeszi-Kékkút-Kővágóörs-Köveskál-Gyulakeszi-Tapolca
-              útvonalon:
-            </p>
-            <p>Térképen:</p>
-            <p>Felvonulás </p>
-            <h4>16:20</h4>
-            <p>Tombola sorsolás, zenés est</p>
-            <h4>23:00</h4>
-            <p>A rendezvény zárása, járművek kiállása</p>
+
+            <table>
+              <tbody>
+                <tr>
+                  <th className="tableWidth"></th>
+                  <th></th>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>6:30-8:30</h4>
+                  </td>
+                  <td>
+                    <strong>Autóbuszok érkezése, beállítása</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>9:00</h4>
+                  </td>
+                  <td>
+                    <strong>Megnyitó</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>9:30-14:00</h4>
+                  </td>
+                  <td>
+                    <p>
+                      Városi nosztalgia járatok közlekednek, melyekre a
+                      Köztársaság téri kijelölt autóbusz megállóhelyen lehet
+                      felszállni előre megváltott menetjeggyel. A járatoknak
+                      útközben nincs megállóhelye, le-, illetve felszállás csak
+                      a végállomáson lehetséges. Az autóbuszok férőhelye
+                      korlátozott, kérjük ezt vegyék figyelembe felszálláskor és
+                      a problémamentes utazásért a kalauzok kéréseit tartsák be.
+                    </p>
+                    <p>A járatok az alábbi útvonalon közlekednek:</p>
+                    <p>Térképen:</p>
+                    <p>Városi nosztalgia autóbusz körjáratok útvonala</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>14:30</h4>
+                  </td>
+                  <td>
+                    <strong>Felvonulás</strong>
+                    <p>
+                      Felvonulás az összes jármű részvételével a a festői
+                      szépségű Káli-Medencében
+                      Tapolca-Gyulakeszi-Kékkút-Kővágóörs-Köveskál-Gyulakeszi-Tapolca
+                      útvonalon:
+                    </p>
+                    <p>Térképen:</p>
+                    <p>Felvonulás </p>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>16:20</h4>
+                  </td>
+                  <td>
+                    <strong>Tombola sorsolás, zenés est</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td valign="top">
+                    <h4>23:00</h4>
+                  </td>
+                  <td>
+                    <strong>A rendezvény zárása, járművek kiállása</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       ) : (

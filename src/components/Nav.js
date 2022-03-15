280188c38b386fb035443e42456e7678b16d9c04
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-03-15 10:12:07 +0100

    add logo to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,18 @@
 import React from "react";
 import MobileNavigation from "./MobileNavigation";
 import Navigation from "./Navigation";
+import logoHu from "../ikarus_tal_white_kor.svg";
 
 const Nav = ({ currentLanguage }) => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">Ikarus Találkozó</div>
+        <div className="logo">
+          <img
+            src={currentLanguage === "hu" ? logoHu : logoHu}
+            alt="language"
+          />
+        </div>
         <MobileNavigation currentLanguage={currentLanguage} />
         <Navigation currentLanguage={currentLanguage} />
       </div>
